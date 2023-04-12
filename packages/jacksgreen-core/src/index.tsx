4ca33e7289a7998ff559698a3a9859fe53cc7b38
Jack Green <jack@beamup.ai>
2023-04-12 19:16:38 +0300

    feat(fix): imports

--- packages/jacksgreen-core/src/index.tsx
+++ packages/jacksgreen-core/src/index.tsx
@@ -1,6 +1,3 @@
 import * as React from "react";
 export { Button, type ButtonProps } from "./Button";
 export { RedButton } from "./RedButton";
-export { GreenButton } from "./GreenButton";
-export { OrangeButton } from "./OrangeButton";
-export { BlueButton } from "./BlueButton";
